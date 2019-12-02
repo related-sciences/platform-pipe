4fd9520623d8bab8d4cff9b353b06144c06ef43d
eric-czech <eric.allen.czech@gmail.com>
2019-12-02 17:29:36 -0500

    scalafmt fix

--- scripts/scoring_pipeline_exec.scala
+++ scripts/scoring_pipeline_exec.scala
@@ -4,7 +4,8 @@
  * /usr/spark-2.4.4/bin/spark-shell --driver-memory 12g --jars target/scala-2.12/ot-scoring_2.12-0.1.jar -i scripts/scoring_pipeline_exec.scala
  */
 import com.relatedsciences.opentargets.pipeline.Pipeline
-new Pipeline(spark)
+
+val p = new Pipeline(spark)
   .runPreprocessing()
   .runScoring()
 System.exit(0)
